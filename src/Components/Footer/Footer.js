65c29a9a9da6b3df1f524362aa0ba180ded05d09
shovan <shovon.bd16@gmail.com>
2021-10-05 02:49:34 -0700

    initial commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    //creating footer
+    <div className="container brdr">
+      <div className="footer">
+      <h5 >Copyright@ 2021 Md Hasan. All Rights Reserved</h5>
+      </div>  
+    </div>
+  );
+};
+
+export default Footer;
